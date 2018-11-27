fc102892c70fa7c7ba88d9116b2c7b602107ee4d
calin.nicoara <calin.nicoara@esolutions.ro>
2018-11-27 18:42:50 +0200

    Stared project app.

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping--edit/shopping-edit.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, RecipeItemComponent, RecipesComponent, RecipeListComponent,
+    RecipeDetailComponent, ShoppingListComponent, ShoppingEditComponent, HeaderComponent
   ],
   imports: [
     BrowserModule
