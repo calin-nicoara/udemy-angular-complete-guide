9123b051119295794c1af60dfca95a3dfd9a482f
Calin Nicoara <calin.nicoara@esolutions.ro>
2019-01-05 14:37:07 +0200

    Finished services part.

---
+++ services-start/src/app/account/account.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {LoggingService} from '../shared/logging.service';
+import {AccountsService} from '../shared/accounts.service';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css']
+})
+export class AccountComponent {
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+
+  constructor(private accountService: AccountsService) {}
+
+  onSetTo(status: string) {
+    this.accountService.updateStatus(this.id, status);
+  }
+}
