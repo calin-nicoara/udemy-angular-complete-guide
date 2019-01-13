7fc3f5dfb9227128e3fb4f262fab165ad6bb135b
Calin Nicoara <calin.nicoara@esolutions.ro>
2019-01-13 13:35:33 +0200

    Finished routing for recipe book.

--- recipe-book/src/app/app-routing.module.ts
+++ recipe-book/src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import {RouterModule, Routes} from '@angular/router';
 import {RecipesComponent} from './recipes/recipes.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
+import {NoRecipeSelectedComponent} from './recipes/no-recipe-selected/no-recipe-selected.component';
+import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
 
 const routes: Routes = [
-  {path: '', component: RecipesComponent},
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
   {path: 'recipes', component: RecipesComponent, children: [
-      {path: ':id', component: RecipeDetailComponent}
+      {path: '', component: NoRecipeSelectedComponent},
+      {path: 'new', component: RecipeEditComponent},
+      {path: ':id', component: RecipeDetailComponent},
+      {path: ':id/edit', component: RecipeEditComponent}
     ]},
   {path: 'shopping-list', component: ShoppingListComponent}
 ];
