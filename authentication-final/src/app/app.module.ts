24c18d4512ccf9cbcacbf95ff3ce180d6a7e9578
Calin Nicoara <calin.nicoara@esolutions.ro>
2019-02-21 23:05:28 +0200

    Restructure modules.

---
+++ authentication-final/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpModule} from '@angular/http';
+
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {ShoppingListService} from './shopping-list/shopping-list.service';
+import {AppRoutingModule} from './app-routing.module';
+import {RecipeService} from './recipes/recipe.service';
+import {DataStorageService} from './shared/data-storage.service';
+import {AuthService} from './auth/auth.service';
+import {AuthGuard} from './auth/auth-guard.service';
+import {RecipesModules} from './recipes/recipe.module';
+import {ShoppingListModule} from './shopping-list/shopping-list.module';
+import {AuthModule} from './auth/auth.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    RecipesModules,
+    ShoppingListModule,
+    AuthModule,
+    AppRoutingModule
+  ],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
